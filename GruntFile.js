3943ca8212e2d366b86ebeeee1bbe70c4efe7b56
steveoh <sgourley@utah.gov>
2014-06-04 15:27:58 -0600

    use moar grunts

--- GruntFile.js
+++ GruntFile.js
@@ -1,49 +1,102 @@
+/* jshint camelcase:false */
 module.exports = function(grunt) {
-    // Project configuration.
+    var jsFiles = 'src/app/**/*.js',
+        gruntFile = 'GruntFile.js',
+        otherFiles = [
+            'src/app/**/*.html',
+            'src/app/**/*.css',
+            'src/index.html',
+            'src/ChangeLog.html'
+        ],
+        jsHintFiles = [jsFiles, gruntFile];
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        connect: {
+            uses_defaults: {}
+        },
         jasmine: {
-            // for embedded map projects...
-            // app: {
-            //   src: ['src/EmbeddedMapLoader.js'],
-            //   options: {
-            //     specs: ['src/app/tests/spec/*.js']
-            //   }
-            // }
-
-            // for regular apps...
             app: {
                 src: ['src/app/run.js'],
                 options: {
                     specs: ['src/app/tests/spec/*.js'],
                     vendor: [
+                        'src/jasmine-favicon-reporter/vendor/favico.js',
+                        'src/jasmine-favicon-reporter/jasmine-favicon-reporter.js',
                         'src/app/tests/jasmineTestBootstrap.js',
-                        // 'http://js.arcgis.com/3.6/',
                         'src/dojo/dojo.js'
-                    ]
+                    ],
+                    host: 'http://localhost:8000'
+                }
+            }
+        },
+        watch: {
+            jshint: {
+                files: jsHintFiles,
+                tasks: ['jshint', 'jasmine:default:build']
+            },
+            src: {
+                files: jsHintFiles.concat(otherFiles),
+                options: {
+                    livereload: true
                 }
             }
         },
         jshint: {
-            files: ['src/app/**/*.js'],
+            files: jsHintFiles,
             options: {
                 jshintrc: '.jshintrc'
             }
         },
-        watch: {
-            files: [
-                'src/app/**/*.js',
-                'src/app/tests/*.html',
-                'src/index.html',
-                'src/user_admin.html'
-            ],
-            tasks: ['jasmine:app:build', 'jshint'],
+        clean: ['dist'],
+        esri_slurp: {
             options: {
-                livereload: true
+                version: '3.9'
             }
         },
-        connect: {
-            uses_defaults: {}
+        dojo: {
+            prod: {
+                options: {
+                    profiles: ['profiles/prod.build.profile.js', 'profiles/build.profile.js'] // Profile for build
+                }
+            },
+            stage: {
+                options: {
+                    profiles: ['profiles/stage.build.profile.js', 'profiles/build.profile.js'] // Profile for build
+                }
+            },
+            options: {
+                dojo: 'src/dojo/dojo.js', // Path to dojo.js file in dojo source
+                releaseDir: '../dist',
+                require: 'src/app/run.js', // Optional: Module to require for the build (Default: nothing)
+                basePath: './src'
+            }
+        },
+        processhtml: {
+            options: {},
+            dist: {
+                files: {
+                    'dist/index.html': ['src/index.html']
+                }
+            }
+        },
+        copy: {
+            main: {
+                src: 'src/ChangeLog.html',
+                dest: 'dist/ChangeLog.html'
+            }
+        },
+        bump: {
+            options: {
+                files: ['package.json', 'src/app/package.json', 'src/app/main.js'],
+                commit: true,
+                commitFiles: ['-a'], // '-a' for all files
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: true,
+                pushTo: 'origin'
+            }
         }
     });
 
@@ -52,7 +105,20 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-esri-slurp');
+    grunt.loadNpmTasks('grunt-processhtml');
+    grunt.loadNpmTasks('grunt-dojo');
+    grunt.loadNpmTasks('grunt-bump');
+
 
     // Default task.
     grunt.registerTask('default', ['jasmine:app:build', 'jshint', 'connect', 'watch']);
+    grunt.registerTask('travis', ['jshint', 'esri_slurp', 'connect', 'jasmine:app']);
+
+    grunt.registerTask('build',
+        ['clean', 'dojo:prod', 'newer:imagemin:dynamic', 'copy', 'processhtml:dist']);
+    grunt.registerTask('stage-build',
+        ['clean', 'dojo:stage', 'newer:imagemin:dynamic', 'copy', 'processhtml:dist']);
 };
--- src/app/tests/jasmineTestBootstrap.js
+++ src/app/tests/jasmineTestBootstrap.js
@@ -1,11 +1,11 @@
 /*jshint unused:false*/
+/* global JasmineFaviconReporter */
 var dojoConfig = {
     isDebug: false,
     isJasmineTestRunner: true, // prevents parser in main.js from running
-    has: {'dojo-undef-api': true}
+    has: {
+        'dojo-undef-api': true
+    }
 };
 
-// override alert to console
-window.alert = function(msg) {
-    console.error('ALERT OVERRIDDEN TO LOG: ' + msg);
-};
+jasmine.getEnv().addReporter(new JasmineFaviconReporter());
