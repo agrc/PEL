ad50d7d3c45a8925cdc251e6a1e529fe9fb266bf
steveoh <sgourley@utah.gov>
2014-06-04 16:14:46 -0600

    css layout and yo cloud request pane

---
+++ src/app/LoginRegisterCloudRequestPane.js
@@ -0,0 +1,44 @@
+define([
+    'dojo/text!./templates/LoginRegisterCloudRequestPane.html',
+
+    'dojo/_base/declare',
+
+    'dijit/_WidgetBase',
+    'dijit/_TemplatedMixin'
+], function(
+    template,
+
+    declare,
+
+    _WidgetBase,
+    _TemplatedMixin
+) {
+    return declare([_WidgetBase, _TemplatedMixin], {
+        // description:
+        //      Overloaded Request page to jump through the cloud hosting hoops
+
+        templateString: template,
+        baseClass: 'login-register-cloud-request-pane',
+
+        // Properties to be sent into constructor
+
+        postCreate: function() {
+            // summary:
+            //      Overrides method of same name in dijit._Widget.
+            // tags:
+            //      private
+            console.log('app.LoginRegisterCloudRequestPane::postCreate', arguments);
+
+            this.setupConnections();
+
+            this.inherited(arguments);
+        },
+        setupConnections: function() {
+            // summary:
+            //      wire events, and such
+            //
+            console.log('app.LoginRegisterCloudRequestPane::setupConnections', arguments);
+
+        }
+    });
+});
