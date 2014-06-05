4465ba1352b351b0c3ca7fb10946a7b0bac2b2f6
steveoh <sgourley@utah.gov>
2014-06-05 17:51:09 -0600

    yo dojo-widget cloud change password

---
+++ src/app/_LoginRegisterCloudLogout.js
@@ -0,0 +1,44 @@
+define([
+    'dojo/text!./templates/_LoginRegisterCloudLogout.html',
+
+    'dojo/_base/declare',
+
+    'dijit/_WidgetBase',
+    'dijit/_TemplatedMixin'
+], function(
+    template,
+
+    declare,
+
+    _WidgetBase,
+    _TemplatedMixin
+) {
+    return declare([_WidgetBase, _TemplatedMixin], {
+        // description:
+        //      Overloaded Logout widget with cloud password restrictions for changing password
+
+        templateString: template,
+        baseClass: 'login-register-cloud-logout',
+
+        // Properties to be sent into constructor
+
+        postCreate: function() {
+            // summary:
+            //      Overrides method of same name in dijit._Widget.
+            // tags:
+            //      private
+            console.log('app._LoginRegisterCloudLogout::postCreate', arguments);
+
+            this.setupConnections();
+
+            this.inherited(arguments);
+        },
+        setupConnections: function() {
+            // summary:
+            //      wire events, and such
+            //
+            console.log('app._LoginRegisterCloudLogout::setupConnections', arguments);
+
+        }
+    });
+});
