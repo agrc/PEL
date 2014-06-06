65cbcb108109c5aa36d363ae9cee7ba004c8bdb7
steveoh <sgourley@utah.gov>
2014-06-05 19:24:28 -0600

    added same thing to change password
    duplicated stuff which stinks. but maybe later

--- src/app/_LoginRegisterCloudLogout.js
+++ src/app/_LoginRegisterCloudLogout.js
@@ -3,42 +3,20 @@ define([
 
     'dojo/_base/declare',
 
-    'dijit/_WidgetBase',
-    'dijit/_TemplatedMixin'
+    'ijit/widgets/authentication/_LoginRegisterLogout',
+    './_CloudPasswordMixin'
 ], function(
     template,
 
     declare,
 
-    _WidgetBase,
-    _TemplatedMixin
+    _LoginRegisterLogout,
+    _CloudPasswordMixin
 ) {
-    return declare([_WidgetBase, _TemplatedMixin], {
+    return declare([_LoginRegisterLogout, _CloudPasswordMixin], {
         // description:
         //      Overloaded Logout widget with cloud password restrictions for changing password
 
-        templateString: template,
-        baseClass: 'login-register-cloud-logout',
-
-        // Properties to be sent into constructor
-
-        postCreate: function() {
-            // summary:
-            //      Overrides method of same name in dijit._Widget.
-            // tags:
-            //      private
-            console.log('app._LoginRegisterCloudLogout::postCreate', arguments);
-
-            this.setupConnections();
-
-            this.inherited(arguments);
-        },
-        setupConnections: function() {
-            // summary:
-            //      wire events, and such
-            //
-            console.log('app._LoginRegisterCloudLogout::setupConnections', arguments);
-
-        }
+        templateString: template
     });
 });
